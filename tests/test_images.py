fbb3c4bc3cf3fd524cfd6c14e835cc3192ae0d5a
Milton Carlos Katoo <mckatoo@gmail.com>
2024-06-13 15:48:59 -0300

    get image using public_id

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,9 +1,9 @@
 import io
+import json
 from unittest.mock import Mock
 
 import cloudinary.uploader as uploader
 import cloudinary.api as api
-
 
 
 def test_send_image_returning_status_201_and_json(client):
@@ -11,11 +11,10 @@ def test_send_image_returning_status_201_and_json(client):
         "public_id": "test_public_id",
         "secure_url": "test_secure_url",
     }
-    mocked_value = {**mocked_result}
-    mocked_value["file"] = (io.BytesIO(b"abcdef"), "test.jpg")  # type: ignore
-    uploader.upload = Mock(return_value=mocked_value)
+    data = {"file": (io.BytesIO(b"some initial text data"), "teste.jpg")}
+    uploader.upload = Mock(return_value=mocked_result)
     response = client.post(
-        "api/image", data=mocked_value, content_type="multipart/form-data"
+        "api/image", data=data, content_type="multipart/form-data"
     )
 
     assert uploader.upload.called
@@ -27,13 +26,15 @@ def test_send_image_returning_status_201_and_json(client):
 
 
 def test_get_image_returning_status_200_and_json(client):
-    mocked_request = {"id": "mocked_public_id"}
+    mocked_request = json.dumps(dict(id="mocked_public_id"))
     mocked_response = {"url": "http://mocked.com/response.jpg"}
-    api.resource = Mock(return_value = mocked_response)
-    response = client.get("api/image",data=mocked_request, content_type="application/json")
-    
+    api.resource = Mock(return_value=mocked_response)
+    response = client.get(
+        "api/image", data=mocked_request, content_type="application/json"
+    )
+
     assert response.status_code == 200
-    # assert response.json == {"url": mocked_response["url"]}
+    assert response.json == {"url": mocked_response["url"]}
 
 
 def test_delete_image_returning_status_204_and_no_content(client):
