7242fce6cfbf6da72a6eb8940e309275ab213898
Milton Carlos Katoo <mckatoo@gmail.com>
2024-06-22 15:58:32 -0300

    initialized projects table

--- configurations/db_config.py
+++ configurations/db_config.py
@@ -1,8 +1,8 @@
 from database import db
 from database.models.images import Images
 from database.models.pages import Pages
-
+from database.models.projects import Projects
 
 def load_db_config():
     db.connect()
-    db.create_tables([Pages, Images])
+    db.create_tables([Pages, Images, Projects])
