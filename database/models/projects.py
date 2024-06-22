f547bef7858c07852be2519df8bf09b02ae2f995
Milton Carlos Katoo <mckatoo@gmail.com>
2024-06-22 16:00:19 -0300

    removed invalid attribute on text field

--- database/models/projects.py
+++ database/models/projects.py
@@ -1,9 +1,3 @@
-# Projects(models.Model):
-#     class Meta:
-#         db_table = "Projects"
-#         verbose_name_plural = "Projects"
-
-
 from peewee import Model, CharField, TextField, DateField, ManyToManyField
 from database import db
 from datetime import datetime
@@ -16,9 +10,9 @@ class Projects(Model):
         table_name = "Projects"
 
     title = CharField(max_length=200, null=False, unique=True)
-    description = TextField(max_length=1500, null=False)
+    description = TextField(null=False)
     snapshot = CharField(max_length=200, null=False)
     repository_link = CharField(null=False)
-    start = DateField(default=datetime.now)
-    last_update = DateField(auto_now=True)
+    start = DateField(default=datetime.now().date())
+    last_update = DateField(default=datetime.now().date())
     # skills = ManyToManyField(Skills)
