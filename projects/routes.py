98a25ddb20a44c4599ee90fb46a819343360b6fd
Milton Carlos Katoo <mckatoo@gmail.com>
2024-06-22 16:00:50 -0300

    featured create project

--- projects/routes.py
+++ projects/routes.py
@@ -1,18 +1,31 @@
-
 from flask import Blueprint, jsonify, request
+from database.models.projects import Projects
 
 projects_routes = Blueprint("projects_routes", __name__)
 project_routes = Blueprint("project_routes", __name__)
 
 
-#  path("project/<int:pk>", GetUpdateDeleteProject.as_view()),
-@project_routes.route("/<int:id>", methods=["GET"])
-def get_project(id: int):
-    return jsonify({"id": id}),200
-
-
 #     re_path("^project/?$", CreateProject.as_view()),
 @project_routes.route("", methods=["POST"])
 def create_project():
-    return jsonify({"id": 1}),201
+    if not request.json:
+        raise
+    data = request.json
+    project = Projects.create(
+        title=data["title"],
+        description=data["description"],
+        snapshot=data["snapshot"],
+        repository_link=data["repositoryLink"],
+        start=data["start"],
+        last_update=data["lastUpdate"],
+    )
+    return jsonify({"id": project.id}), 201
+
+
+#  path("project/<int:pk>", GetUpdateDeleteProject.as_view()),
+@project_routes.route("/<int:id>", methods=["GET"])
+def get_project(id: int):
+    return jsonify({}), 200
+
+
 #     re_path("^projects/?$", ListProjects.as_view()),
--- tests/projects/test_get_project_by_id.py
+++ tests/projects/test_get_project_by_id.py
@@ -1,25 +1,42 @@
 import json
+from database.models.projects import Projects
+import uuid
+from camel_converter import dict_to_snake
+
 
 def test_create_project_returning_status_201_and_id(client):
-    mocked_request = json.dumps(
-        {
-            "from": "sender@mail.com",
-            "to": "reciper@mail.com",
-            "message": "hello",
-        }
-    )
+    random_id = uuid.uuid1()
+    mocked_data = {
+        "title": f"Title test {random_id}",
+        "description": f"Description test  {random_id}",
+        "snapshot": f"http://snapshot.com/image-{random_id}.jpg",
+        "repositoryLink": f"http://repository.com/my{random_id}.git",
+        "start": "2024/03/22",
+        "lastUpdate": "2024/06/13",
+    }
     response = client.post(
-        "api/project", content_type="application/json",
-        data=mocked_request
+        "api/project",
+        content_type="application/json",
+        data=json.dumps(mocked_data),
     )
+    created_project = Projects.get(Projects.title == mocked_data["title"])
 
     assert response.status_code == 201
-    assert response.json == {"id": 1}
+    assert response.json == {"id": created_project.id}
+
 
 def test_get_project_by_id_returning_status_200_and_json(client):
-    response = client.get(
-        "api/project/4", content_type="application/json"
-    )
+    random_id = uuid.uuid1()
+    mocked_data = {
+        "title": f"Title test {random_id}",
+        "description": f"Description test  {random_id}",
+        "snapshot": f"http://snapshot.com/image-{random_id}.jpg",
+        "repositoryLink": f"http://repository.com/my{random_id}.git",
+        "start": "2024/03/22",
+        "lastUpdate": "2024/06/13",
+    }
+    Projects.create(**json.load(dict_to_snake(mocked_data)))
+    # response = client.get("api/project/4", content_type="application/json")
 
-    assert response.status_code == 200
-    assert response.json == {"id": 4}
+    # assert response.status_code == 200
+    # assert response.json == {"id": 4}
