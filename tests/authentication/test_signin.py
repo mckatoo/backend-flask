3a7435cab529f9e5e9e10d5785721b4757e14366
Milton Carlos Katoo <mckatoo@gmail.com>
2024-06-28 21:12:28 -0300

    signin tests

--- tests/authentication/test_signin.py
+++ tests/authentication/test_signin.py
@@ -3,6 +3,7 @@ from flask import json
 from configurations import envs_config
 from database.models.users import Users
 import jwt
+from datetime import datetime, timedelta, timezone
 
 
 def test_success_on_signin(client):
@@ -32,3 +33,33 @@ def test_success_on_signin(client):
     assert response.status_code == 200
     assert access_token_payload["id"] == created_user.id
     assert refresh_token_payload["id"] == created_user.id
+    assert access_token_payload["exp"] == int(
+        (datetime.now(tz=timezone.utc) + timedelta(minutes=10)).timestamp()
+    )
+    assert refresh_token_payload["exp"] == int(
+        (datetime.now(tz=timezone.utc) + timedelta(minutes=60)).timestamp()
+    )
+
+def test_fail_on_request_signin_with_invalid_credentials(client):
+    mocked_user = {
+        "email": "invalid@user.com",
+        "password": "123456",
+    }
+
+    response = client.post(
+        "api/auth/sign-in",
+        data=json.dumps(mocked_user),
+        content_type="application/json",
+    )
+
+    assert response.status_code == 401
+    assert response.json == {"error": "Unauthorized"}
+
+def test_bad_request_error_when_request_without_credentials(client):
+    response = client.post(
+        "api/auth/sign-in",
+        content_type="application/json",
+    )
+
+    assert response.status_code == 400
+    assert response.json == {"error": "Bad Request"}
