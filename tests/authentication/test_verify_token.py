924cdf38e5e0cfb5292f4268aad5c2cdc94e2b08
Milton Carlos Katoo <mckatoo@gmail.com>
2024-06-28 21:13:41 -0300

    verity_token failed tests

---
+++ tests/authentication/test_verify_token.py
@@ -0,0 +1,12 @@
+
+def test_success_on_verify_token(client):
+    assert False
+
+def test_should_be_error_401_when_request_with_invalid_token(client):
+    assert False
+
+def test_should_be_error_401_when_request_with_expired_token(client):
+    assert False
+
+def test_should_be_error_400_when_request_without_token(client):
+    assert False
