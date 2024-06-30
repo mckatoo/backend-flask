40f1bbde80fcf3545fd9862742aebe9f4ec8743c
Milton Carlos Katoo <mckatoo@gmail.com>
2024-06-30 10:45:08 -0300

    sign out tests

--- tests/authentication/test_signout.py
+++ tests/authentication/test_signout.py
@@ -1,8 +1,46 @@
-def test_when_sigout_return_200_status_code(client):
-    assert False
+from datetime import datetime, timedelta, timezone
+from uuid import uuid1
 
-def test_when_sigout_with_success_add_token_in_blacklist(client):
-    assert False
+import jwt
 
-def test_should_return_401_status_code_when_request_restrict_route_after_signout(client):
-    assert False
+from configurations import envs_config
+from database.models.blacklist import Blacklist
+from database.models.users import Users
+
+
+def test_when_sigout_return_200_status_code_and_add_on_blacklist(client):
+    random_id = uuid1()
+    mocked_user = {
+        "username": f"user_{random_id}",
+        "email": f"user{random_id}@mail.com",
+        "password": "123456",
+    }
+    user = Users.create(**mocked_user)
+
+    access_token = jwt.encode(
+        {
+            "id": user.id,
+            "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=10),
+        },
+        envs_config.SECRET_KEY,
+    )
+    response = client.post(
+        "api/auth/sign-out",
+        headers={"authentication": f"Bearer {access_token}"},
+        content_type="application/json",
+    )
+    blacklist_token = Blacklist.get_or_none(Blacklist.token == access_token)
+
+    assert response.status_code == 200
+    assert blacklist_token is not None
+
+
+def test_should_return_401_status_code_when_request_restrict_route_after_signout(
+    client,
+):
+    response = client.post(
+        "/api/user",
+        content_type="application/json",
+    )
+
+    assert response.status_code == 401
