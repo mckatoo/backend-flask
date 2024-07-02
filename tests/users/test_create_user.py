b8ff50c1a0cd529daefc41f7ab803044163aca26
Milton Carlos Katoo <mckatoo@gmail.com>
2024-07-02 15:38:09 -0300

    featured user creation

--- tests/users/test_create_user.py
+++ tests/users/test_create_user.py
@@ -1,8 +1,30 @@
-def test_create_user_and_return_201_status_code(client):
-    assert False
+import json
+
+from database.models.users import Users
+from tests.utils import generate_mocked_user_data
+
+
+def test_create_user_returning_user_id_and_201_status_code(client):
+    mocked_user_data = generate_mocked_user_data()
+    existent_user_data = generate_mocked_user_data()
+    existent_user = Users.create(**existent_user_data)
+    access_token, _ = existent_user.generate_tokens()
+
+    response = client.post(
+        "api/user",
+        data=json.dumps(mocked_user_data),
+        headers={"authentication": f"Bearer {access_token}"},
+        content_type="application/json",
+    )
+    created_user = Users.get(Users.email == mocked_user_data["email"])
+
+    assert response.status_code == 201
+    assert response.json == {"id": created_user.id}
+
 
 def test_error_400_when_request_creation_without_data(client):
     assert False
+
 
 def test_error_400_when_request_creation_with_invalid_data(client):
     assert False
--- users/routes.py
+++ users/routes.py
@@ -1,6 +1,4 @@
-from camel_converter import dict_to_camel, dict_to_snake
 from flask import Blueprint, jsonify, request
-from playhouse.shortcuts import model_to_dict
 
 from database.models.users import Users
 from middlewares.verify_token import verify_token_middleware
@@ -12,4 +10,8 @@ user_routes = Blueprint("user_routes", __name__)
 @user_routes.route("", methods=["POST"])
 @verify_token_middleware
 def create_user():
-    return jsonify({}), 201
+    try:
+        created_user = Users.create(**request.json)
+        return jsonify({"id": created_user.id}), 201
+    except Exception as e:
+        return jsonify({"error": "Unexpected Error"}), 500
