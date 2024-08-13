320e651e337e8e8bc820d602cb2fd449958d72bd
Milton Carlos Katoo <mckatoo@gmail.com>
2024-08-13 19:22:51 -0300

    verify headers at the beginning

--- middlewares/verify_token.py
+++ middlewares/verify_token.py
@@ -13,19 +13,21 @@ def decode_token(token: str) -> dict:
 def verify_token_middleware(func):
     @wraps(func)
     def decorated_func(*args, **kargs):
+        if "authorization" not in request.headers:
+            return jsonify({"error": "Unauthorized"}), 401
         try:
             token = str(request.headers["authorization"]).removeprefix(
                 "Bearer "
             )
             decoded_token = decode_token(token)
             in_blacklist = bool(Blacklist.get_or_none(token=token))
-            is_expired = (
+            is_expired = bool(
                 decoded_token["exp"] < datetime.now(tz=timezone.utc).timestamp()
             )
 
-            if not in_blacklist and not is_expired:
-                return func(*args, **kargs)
-            return jsonify({"error": "Unauthorized"}), 401
+            if in_blacklist or is_expired:
+                return jsonify({"error": "Unauthorized"}), 401
+            return func(*args, **kargs)
         except Exception as e:
             if (
                 str(e).__contains__("HTTP_AUTHENTICATION")
